38105f1152f05516191db465b5a4889c240503af
Midhunkumar-se <midhu025@gmail.com>
2024-04-17 13:21:47 +0530

    removed unwanted command

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,15 +1,12 @@
 const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
+
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     // set default
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong try again later",
   };
-
-  // if (err instanceof CustomAPIError) {
-  //   return res.status(err.statusCode).json({ msg: err.message });
-  // }
 
   if (err.name === "ValidationError") {
     customError.msg = Object.values(err.errors)
@@ -25,7 +22,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.statusCode = 400;
   }
 
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id: ${err.value}`;
+    customError.statusCode = 404;
+  }
+
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
